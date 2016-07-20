418d77f8109b4ef1570fc9941407ee52e63b95d3
yuriMartins <ysm281@gmail.com>
2016-07-20 16:04:53 -0300

    Toma ai

---
+++ CompilaTest/src/model/uefs/br/Token.java
@@ -0,0 +1,19 @@
+package model.uefs.br;
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yuri Silva
+ */
+public class Token {
+    
+    private ArrayList<String>leximas = new ArrayList<String>();
+    
+}
